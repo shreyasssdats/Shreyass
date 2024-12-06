5918d6b7beebfa44dab956357de7b44db8d9adee
Shreyas Datar <160137641+shreyasssdats@users.noreply.github.com>
2024-12-06 18:25:57 +0530

    Add files via upload
    update

---
+++ Socket/src/server.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class server {
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss=new ServerSocket(8888);
+        Socket s=ss.accept();
+
+
+        DataInputStream din=new DataInputStream(s.getInputStream());
+        DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        String str ="",str2="";
+        while (!str.equals("stop"))
+        {
+            str=din.readUTF();
+            System.out.println("Client says:" + str);
+            str2=br.readLine();
+            dout.writeUTF(str2);
+            dout.flush();
+        }
+        din.close();
+        s.close();
+        ss.close();
+
+    }
+}
